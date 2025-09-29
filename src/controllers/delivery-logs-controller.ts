3db99f72f75a4f280493b721fa9f7fc946b86bd4
Eduarda Pontel <eduardapontel@gmail.com>
2025-09-30 00:11:11 +0100

    final commit

---
+++ src/controllers/delivery-logs-controller.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from 'express';
+import { z } from 'zod';
+import { prisma } from '@/database/prisma';
+import { AppError } from '@/utils/AppError';
+
+class DeliveryLogsController {
+    async create(request: Request, response: Response) {
+        const bodySchema = z.object({
+            delivery_id: z.string().uuid(),
+            description: z.string(),
+        });
+
+        const { delivery_id, description } = bodySchema.parse(request.body);
+
+        const delivery = await prisma.delivery.findUnique({
+            where: { id: delivery_id },
+        });
+
+        if (!delivery) {
+            throw new AppError('Delivery not found', 404);
+        }
+
+        if (delivery.status === 'delivered') {
+            throw new AppError('Delivery already delivered');
+        }
+
+        if (delivery.status === 'processing') {
+            throw new AppError('Change status to shipped');
+        }
+
+        await prisma.deliveryLog.create({
+            data: {
+                delivery_id,
+                description,
+            },
+        });
+        return response.status(201).json();
+    }
+
+    async show(request: Request, response: Response) {
+        const paramsSchema = z.object({
+            delivery_id: z.string().uuid(),
+        });
+
+        const { delivery_id } = paramsSchema.parse(request.params);
+
+        const delivery = await prisma.delivery.findUnique({
+            where: { id: delivery_id },
+            include: { user: true, logs: true },
+        });
+
+        if (request.user?.role === 'customer' && request.user.id !== delivery?.user_id) {
+            throw new AppError('Unauthorized', 401);
+        }
+
+        return response.json(delivery);
+    }
+}
+
+export { DeliveryLogsController };
---
+++ src/controllers/sessions-controller.ts
@@ -0,0 +1,45 @@
+import { AppError } from '@/utils/AppError';
+import { Request, Response } from 'express';
+import { prisma } from '@/database/prisma';
+import { authConfig } from '@/configs/auth';
+import { sign } from 'jsonwebtoken';
+import { compare } from 'bcrypt';
+import { z } from 'zod';
+
+class SessionsController {
+    async create(request: Request, response: Response) {
+        const bodySchema = z.object({
+            email: z.string().email(),
+            password: z.string().min(6),
+        });
+
+        const { email, password } = bodySchema.parse(request.body);
+
+        const user = await prisma.user.findFirst({
+            where: { email },
+        });
+
+        if (!user) {
+            throw new AppError('Email or password incorrect', 401);
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new AppError('Email or password incorrect', 401);
+        }
+
+        const { secret, expiresIn } = authConfig.jwt;
+
+        const token = sign({ role: user.role ?? 'customer' }, secret, {
+            subject: user.id,
+            expiresIn,
+        });
+
+        const { password: hashedPassword, ...userWithoutPassword } = user;
+
+        return response.json({ token, user: userWithoutPassword });
+    }
+}
+
+export { SessionsController };
